7216011e59709394bf4b59a4c8baeeb4660e5ec4
Gabriel Reynes <gabrielreynes@gmail.com>
2022-02-28 21:47:32 -0500

    Neighbour Array to IEnumerable

--- Utilities/Search/SearchProblem.cs
+++ Utilities/Search/SearchProblem.cs
@@ -3,16 +3,16 @@
 public class SearchProblem<T>
 {
     public readonly Func<T> GetStartState;
-    public readonly Func<T, T[]> NeighboursFetch;
+    public readonly Func<T, IEnumerable<T>> NeighboursFetch;
     public Predicate<T> EndStatePredicate;
     public Predicate<T> ObstaclePredicate;
     public Func<T, int> Heuristic;
 
-    public SearchProblem(T startState, Func<T, T[]> neighboursFetch) : this(() => startState, neighboursFetch)
+    public SearchProblem(T startState, Func<T, IEnumerable<T>> neighboursFetch) : this(() => startState, neighboursFetch)
     {
     }
 
-    public SearchProblem(Func<T> getStartState, Func<T, T[]> neighboursFetch)
+    public SearchProblem(Func<T> getStartState, Func<T, IEnumerable<T>> neighboursFetch)
     {
         GetStartState = getStartState;
         NeighboursFetch = neighboursFetch;
