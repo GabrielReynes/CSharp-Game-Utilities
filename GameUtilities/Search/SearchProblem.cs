e2142af95463918aad4450ee58c67142828a2351
Gabriel Reynes <gabrielreynes@gmail.com>
2022-05-30 13:57:39 +0200

    Documenting SearchProblem class use

--- GameUtilities/Search/SearchProblem.cs
+++ GameUtilities/Search/SearchProblem.cs
@@ -1,15 +1,17 @@
 ﻿namespace GameUtilities.Search
 {
-
+    /*
+     * Represents a problem which defines a set of method used to simulate a displacement inside your search graph.
+     */
     public class SearchProblem<T>
     {
-        public readonly Func<T> GetStartState;
-        public readonly Func<T, IEnumerable<T>> NeighboursFetch;
-        public Predicate<T> EndStatePredicate;
-        public Predicate<T> ObstaclePredicate;
-        public Func<T, T, int> Cost;
-        public Func<T, int> Heuristic;
-        public Action<T, T> CoupleAction; //Method which will be called for every node/neighbour couple
+        public readonly Func<T> GetStartState; // Method which returns the starting state
+        public readonly Func<T, IEnumerable<T>> NeighboursFetch; // Method which returns a set of neighbours given a state
+        public Predicate<T> EndStatePredicate; // Method which determines whether a given state is an end state
+        public Predicate<T> ObstaclePredicate; // Method which determines whether a given state is an obstacle to be avoided
+        public Func<T, T, int> Cost; // Method which is used to measure the cost of displacement between two states
+        public Func<T, int> Heuristic; // Method which is user to determine the heuristic cost between two states
+        public Action<T, T> CoupleAction; // Method which will be called for every node/neighbour couple
 
         public SearchProblem(T startState, Func<T, IEnumerable<T>> neighboursFetch) : this(() => startState,
             neighboursFetch)
